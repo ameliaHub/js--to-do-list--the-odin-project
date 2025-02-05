aa5c4efa383ea7de619e9d1967ed14cb55249a9f
pilarvermu <pilaribermu@gmail.com>
2025-02-05 22:18:00 +0100

    Añadida logica de clase toDo

---
+++ src/todo.js
@@ -0,0 +1,17 @@
+export class ToDo{
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    toggleCompleted() {
+        this.completed = !this.completed;
+    }
+
+    changePriority(newPriority) {
+        this.priority = newPriority
+    }
+}
