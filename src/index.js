3e5eae05ddd3d09a0d31d33e54c18e9ee5105092
pilarvermu <pilaribermu@gmail.com>
2025-02-08 11:29:53 +0100

    Añadida instancia todolist e importada funcion DOM, añadida prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { ToDo } from "./todo";
 import { Project } from "./project";
 
+import { ToDoList } from "./todolist";
+import { setUpDom } from "./dom";
+
 console.log("HOLA");
 
 //const globalToDos = [];
@@ -9,6 +12,7 @@ console.log("HOLA");
 //    return globalToDos;
 //}
 
+/*
 const project = new Project("Prueba");
 
 const toDo1 = new ToDo('Comprar leche', 'Comprar leche en el supermercado', '2025-02-05', 'alta'); 
@@ -30,3 +34,26 @@ console.log({...project, toDos: JSON.parse(JSON.stringify(project.toDos))});
 project.toggleCompleted("Estudiar para el examen");
 console.log("Después de marcar completo");
 console.log({...project, toDos: JSON.parse(JSON.stringify(project.toDos))});
+
+*/
+
+const myToDoList = new ToDoList();
+
+//prueba
+// Crear algunos ToDos de prueba
+const toDo1 = new ToDo("Comprar leche", "Ir al supermercado", "2025-02-10", "Alta");
+const toDo2 = new ToDo("Hacer ejercicio", "Ir al gimnasio", "2025-02-12", "Media");
+const toDo3 = new ToDo("Estudiar JavaScript", "Repasar closures y promesas", "2025-02-15", "Baja");
+
+myToDoList.addToDo(toDo1);
+myToDoList.addToDo(toDo2);
+myToDoList.addToDo(toDo3);
+
+const project1 = new Project("Proyecto de trabajo");
+project1.addToDo(toDo1);
+
+
+setUpDom(myToDoList);
+
+
+
